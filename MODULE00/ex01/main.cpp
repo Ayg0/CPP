5fcc30e510103df36968ebe55d6b22ebce228dc0
Taha Ed-dafili <ted-dafi@e2r9p2.1337.ma>
2022-12-07 12:02:52 +0100

    CPP01

--- MODULE00/ex01/main.cpp
+++ MODULE00/ex01/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
 #include "phonebook.hpp"
 
---
+++ MODULE01/ex00/Zombie.hpp
@@ -0,0 +1,14 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <iostream>
+
+
+class Zombie
+{
+private:
+	std::string	name;
+public:
+	Zombie(/* args */);
+	~Zombie();
+};
+#endif
