2ca11fb9e03ffb53760fe85d63df4b982fb7c1ca
Taha Ed-dafili <ted-dafi@e2r9p10.1337.ma>
2022-12-28 12:52:17 +0100

    ww

---
+++ MODULE02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+class Fixed
+{
+private:
+	int					nu_value;
+	static const int	bits;
+public:
+	Fixed();
+	Fixed(const Fixed &a);
+	Fixed	&operator = (const Fixed &a);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	~Fixed();
+};
+
+# endif
