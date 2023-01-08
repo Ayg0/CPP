ef06236d2cc53fd614d7d47d0124c22505ac080e
Taha Ed-dafili <ted-dafi@e2r6p12.1337.ma>
2023-01-08 10:15:37 +0100

    nice

--- MODULE02/ex00/main.cpp
+++ MODULE02/ex00/main.cpp
@@ -1,3 +1,13 @@
-int	main(){
-	
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
