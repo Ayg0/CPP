2c416c0063737068876606bcf73e8f7b73c89d4e
Taha Ed-dafili <ted-dafi@e2r5p11.1337.ma>
2023-01-19 09:19:22 +0100

    nice

--- MODULE02/ex02/main.cpp
+++ MODULE02/ex02/main.cpp
@@ -8,7 +8,7 @@ std::ostream &operator << (std::ostream &os, const Fixed &a)
 
 int main( void ) {
 	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const b( Fixed( 5.5f ) * Fixed( 3 ) );
 	//Fixed c(3);
 
 	std::cout << a << std::endl;
@@ -19,5 +19,6 @@ int main( void ) {
 	std::cout << b << std::endl;
 	//std::cout << c * Fixed(2) << std::endl;
 	std::cout << Fixed::max(a, b) << "\n" << Fixed::min(a, b) << std::endl;
+	//std::cout << b / c << std::endl;
 	return 0;
 }
